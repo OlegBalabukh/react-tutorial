a28eca1f5814b402785cbbc582a4a455d4296a1b
obalab <obalab@softserveinc.com>
2019-07-01 17:40:37 +0300

    Updates

---
+++ precache-manifest.2b5da2639be73d11a2041c94da90b1ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9925b45c410203ca3078e9ddaed135",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "3be241f4e81bfe6a9a5f",
+    "url": "/react-tutorial/static/css/main.545ae54c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "3be241f4e81bfe6a9a5f",
+    "url": "/react-tutorial/static/js/main.b3fbcae7.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
