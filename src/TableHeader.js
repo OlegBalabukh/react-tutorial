4d147d35a88793a1ca9a097c4153e616048c35da
obalab <obalab@softserveinc.com>
2019-07-01 16:52:50 +0300

    commit all changes to react-tutorial

---
+++ src/TableHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr className="tableHeaderX">
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  )
+}
+
+export default TableHeader
